2b3104cc4d5b1a755de18a1223b6ce81d1245c62
Tomasz Ostaszewski <ost.tomasz@gmail.com>
2024-11-24 12:31:06 +0100

    rendering imgs

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
+import css from "./ImageGallery.module.css";
+
 const APIKEY = "S9B4Xc46HDCuoMTyqKnfQ6wPMSxgCiQjz9AMPlSbrsI";
 const APIURL = `https://api.unsplash.com/photos/?client_id=${APIKEY}`;
 
@@ -11,21 +13,25 @@ export const ImageGallery = () => {
     const getImgs = async () => {
       const response = await axios.get(APIURL);
       setGallery(response.data);
-      console.log("gallery:", gallery);
     };
     getImgs();
   }, []);
 
   return (
-    <ul>
-      {gallery.map((img) => {
+    <ul className={css.gallery}>
+      {gallery.map((img) => (
         // {
         //   console.log("img:", img);
         // }
         <li key={img.id}>
-          <img src={img.urls.small} alt={img.alt_description} />
-        </li>;
-      })}
+          <img
+            src={img.urls.small}
+            alt={img.alt_description}
+            height={300}
+            width={350}
+          />
+        </li>
+      ))}
     </ul>
   );
 };
