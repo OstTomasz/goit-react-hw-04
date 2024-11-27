7b37a73d991f4c37b067e1b5983b2b4577113fd6
Tomasz Ostaszewski <ost.tomasz@gmail.com>
2024-11-27 19:21:48 +0100

    toaster load

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,9 +1,14 @@
 import css from "./SearchBar.module.css";
 import { CiSearch } from "react-icons/ci";
+import { Toaster } from "react-hot-toast";
 
 export const SearchBar = () => {
   return (
     <header className={css.header}>
+      <div>
+        <Toaster />
+      </div>
+      {/* add toast */}
       <form className={css.form}>
         <button className={css.button} type="submit">
           <CiSearch size={20} />
